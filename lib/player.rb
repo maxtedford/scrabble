84944bfb215c28750ac1f30009cb2f4f50b0acc7
Max Tedford <max.tedford@gmail.com>
2015-05-03 10:17:47 -0600

    added a 'play' method to the player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,18 @@
 require './lib/scrabble'
 require 'pry'
 
-class Player
-attr_reader :total_score
+class Player < Scrabble
+attr_reader :player_score, :name, :player_words
 
-  def initialize
-    @total_score = 0
+  def initialize(name)
+    @name = name
+    @player_score = 0
+    @player_words = []
+  end
+
+  def play(word)
+    @player_score += score(word)
+    @player_words << word
+    puts "#{@name}'s new score is #{@player_score}."
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -6,7 +6,7 @@ require 'minitest/pride'
 class PlayerTest < Minitest::Test
 
   def setup
-    @player = Player.new
+    @player = Player.new("Max")
     @scrabble = Scrabble.new
   end
 
@@ -15,11 +15,12 @@ class PlayerTest < Minitest::Test
   end
 
   def test_the_total_score_defaults_to_zero
-    assert_equal 0, @player.total_score
+    assert_equal 0, @player.player_score
   end
 
   def test_that_the_score_can_be_accumulated_and_returned
-    @scrabble.score("A")
-    assert_equal 1, @scrabble.total_score
+    @player.play("A")
+    assert_equal 1, @player.player_score
+    assert "Max's new score is 1."
   end
 end
