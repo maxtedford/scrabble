893e7ce212505200662daa1604e018953d4dde42
Max Tedford <max.tedford@gmail.com>
2015-05-03 11:22:37 -0600

    added methods to track each word played and each score per turn

--- lib/player.rb
+++ lib/player.rb
@@ -2,17 +2,22 @@ require './lib/scrabble'
 require 'pry'
 
 class Player < Scrabble
-attr_reader :player_score, :name, :player_words
+attr_reader :player_score, :player_scores, :name, :player_words
 
   def initialize(name)
     @name = name
     @player_score = 0
+    @player_scores = []
     @player_words = []
   end
 
   def play(word)
     @player_score += score(word)
+    @player_scores << score(word)
     @player_words << word
-    puts "#{@name}'s new score is #{@player_score}."
+  end
+
+  def print_current_score
+    "#{name}'s current score is #{player_score}."
   end
 end
